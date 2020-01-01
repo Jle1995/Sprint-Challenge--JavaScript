04b69382faf523b0cb3a94ea4d4080b9a0d63f55
Jle1995 <31541518+Jle1995@users.noreply.github.com>
2019-12-31 23:55:53 -0500

    JavaScript sprint challenge complete the 2nd time
    All of my js files are complete. You can check them again to see if they meet MVP or not.

---
+++ functions.js
@@ -0,0 +1,68 @@
+// ==== Callbacks ====  
+
+/* Step 1: Create a higher-order function
+  * Create a higher-order function named consume with 3 parameters: a, b and cb
+  * The first two parameters can take any argument (we can pass any value as argument)
+  * The last parameter accepts a callback
+  * The consume function should return the invocation of cb, passing a and b into cb as arguments
+*/
+
+function HigherOrderFunction(Num1, Num2) {
+    return (Num1 * Num2) / (Num1 - Num2);
+}
+
+function consume(a, b, cb) {
+  a = 15, b = 10;
+  cb(a,b);
+}
+
+consume(15,10,HigherOrderFunction);
+
+/* Step 2: Create several functions to callback with consume();
+  * Create a function named add that returns the sum of two numbers
+  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+*/
+
+function add(Num1, Num2) {
+    var Num1, Num2;
+    return Num1 + Num2;
+}
+
+function multiply(Num1, Num2) {
+    var Num1, Num2;
+    return Num1 * Num2;
+}
+
+function greeting() {
+  var firstName, lastName;
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
+
+/* Step 3: Check your work by un-commenting the following calls to consume(): */
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
+// ==== Closures ==== 
+
+// Explain in your own words why nestedfunction can access the variable internal.
+
+// Explanation: 
+
+// The variable internal is inside "myFunction()" and "nestedFunction()" is inside "myFunction()".
+// The "nestedFunction()" gets called and "console.log" variable internal.
+ 
+
+const external = "I'm outside the function";
+
+function myFunction() {
+  console.log(external);
+  const internal = "Hello! I'm inside myFunction!";
+
+  function nestedFunction() {
+    console.log(internal);
+  };
+  nestedFunction();
+}
+myFunction();
